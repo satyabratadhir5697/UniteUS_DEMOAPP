13f3491401a0760f7972e3eefe5f3eab53e09bd1
@satyabratadhir5697 <satyabratadhir97@gmail.com>
2024-09-10 21:10:14 +0530

    added apikeyinterceptor for validation

--- src/main/java/com/uniteUs/Config/ApiKeyInterceptor.java
+++ src/main/java/com/uniteUs/Config/ApiKeyInterceptor.java
@@ -1,5 +1,7 @@
 package com.uniteUs.Config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,6 +14,8 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class ApiKeyInterceptor implements HandlerInterceptor {
 
+	private static final Logger logger = LoggerFactory.getLogger(HandlerInterceptor.class);
+
     private final ApiKeyService apiKeyService;
 
     public ApiKeyInterceptor(ApiKeyService apiKeyService) {
@@ -21,12 +25,15 @@ public class ApiKeyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String apiKey = request.getHeader("API-Key");
+        
 
         if (apiKey == null || !apiKeyService.validateApiKey(apiKey)) {
+        	
+        	logger.info("Api key is not correct called from TLRD "+apiKey);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
-
+        logger.info("Api key is same with DB:"+apiKey);
         return true;
     }
 }
